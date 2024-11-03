6b1b913f5ae6942d770ececa79050f800e9fc1d7
Michael Ferguson <mfergs7@gmail.com>
2024-11-03 09:27:56 -0500

    Imported upstream version '2.2.2' of 'upstream'

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Branching:
 - ROS2:
    - The [ros2](https://github.com/ros-drivers/openni2_camera/tree/ros2) branch supports Jazzy and later
    - The [iron](https://github.com/ros-drivers/openni2_camera/tree/iron) branch supports Humble to Iron
-   - openni2_launch has NOT been ported since proper lazy subscribers are still not possible in ROS2
+   - openni2_launch has NOT been ported yet
 
 ## Developer document
    - [docs.ros.org/openni2_launch](http://docs.ros.org/en/melodic/api/openni2_launch/html/)
@@ -40,14 +40,11 @@ ros2 launch openni2_camera camera_with_cloud.launch.py
  * The nodelet has been refactored into an rclcpp component called
    "openni2_wrapper::OpenNI2Driver". See the launch folder for an example
    of how to start this.
- * Since most components in image_proc/depth_image_proc lack lazy pub/sub,
-   the advanced processing graphs in rgbd_launch and openni2_launch are not
-   currently feasible. It is recommended to create a launch file with the
-   specific pipeline you want. See the launch folder for an example.
+ * rgbd_launch and openni2_launch have not yet been ported (although it
+   is now possible since lazy pub/sub is implemented). It is recommended
+   to create a launch file with the specific pipeline you want.
+   See the launch folder for an example.
 
 ## Known Issues
 
- * There are currently no subscriber connect/disconnect callbacks in ROS2.
-   This package implements a lazy publisher by running a 1Hz update loop
-   and seeing if there are new subscribers.
  * Using "use_device_time" is currently broken.
--- openni2_camera/src/openni2_driver.cpp
+++ openni2_camera/src/openni2_driver.cpp
@@ -139,11 +139,6 @@ void OpenNI2Driver::periodic()
     initialized_ = true;
   }
 
-  // TODO: check subscriber counts, enable cameras
-  colorConnectCb();
-  depthConnectCb();
-  irConnectCb();
-
   if (enable_reconnect_)
     monitorConnection();
 }
@@ -162,18 +157,45 @@ void OpenNI2Driver::advertiseROSTopics()
   // Asus Xtion PRO does not have an RGB camera
   if (device_->hasColorSensor())
   {
-    pub_color_ = it.advertiseCamera("rgb/image_raw", 1);
+    // Create publisher with connect callback
+    rclcpp::PublisherOptions pub_options;
+    pub_options.event_callbacks.matched_callback =
+      [this](rclcpp::MatchedInfo&)
+      {
+        colorConnectCb();
+      };
+    rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
+    custom_qos.depth = 1;
+    pub_color_ = image_transport::create_camera_publisher(this, "rgb/image_raw", custom_qos, pub_options);
   }
 
   if (device_->hasIRSensor())
   {
-    pub_ir_ = it.advertiseCamera("ir/image", 1);
+    // Create publisher with connect callback
+    rclcpp::PublisherOptions pub_options;
+    pub_options.event_callbacks.matched_callback =
+      [this](rclcpp::MatchedInfo&)
+      {
+        irConnectCb();
+      };
+    rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
+    custom_qos.depth = 1;
+    pub_ir_ = image_transport::create_camera_publisher(this, "ir/image_raw", custom_qos, pub_options);
   }
 
   if (device_->hasDepthSensor())
   {
-    pub_depth_raw_ = it.advertiseCamera("depth/image_raw", 1);
-    pub_depth_ = it.advertiseCamera("depth/image", 1);
+    // Create publisher with connect callback
+    rclcpp::PublisherOptions pub_options;
+    pub_options.event_callbacks.matched_callback =
+      [this](rclcpp::MatchedInfo&)
+      {
+        depthConnectCb();
+      };
+    rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
+    custom_qos.depth = 1;
+    pub_depth_raw_ = image_transport::create_camera_publisher(this, "depth_raw/image", custom_qos, pub_options);
+    pub_depth_ = image_transport::create_camera_publisher(this, "depth/image", custom_qos, pub_options);
     pub_projector_info_ = this->create_publisher<sensor_msgs::msg::CameraInfo>("projector/camera_info", 1);
   }
 
@@ -410,7 +432,6 @@ void OpenNI2Driver::colorConnectCb()
   }
   std::lock_guard<std::mutex> lock(connect_mutex_);
 
-  // This does not appear to work
   color_subscribers_ = pub_color_.getNumSubscribers() > 0;
 
   if (color_subscribers_ && !device_->isColorStreamStarted())
@@ -463,7 +484,6 @@ void OpenNI2Driver::depthConnectCb()
   }
   std::lock_guard<std::mutex> lock(connect_mutex_);
 
-  // These does not appear to work
   depth_subscribers_ = pub_depth_.getNumSubscribers() > 0;
   depth_raw_subscribers_ = pub_depth_raw_.getNumSubscribers() > 0;
   projector_info_subscribers_ = pub_projector_info_->get_subscription_count() > 0;
@@ -493,8 +513,6 @@ void OpenNI2Driver::irConnectCb()
   }
   std::lock_guard<std::mutex> lock(connect_mutex_);
 
-  // This does not appear to work
-  // ir_subscribers_ = pub_ir_.getNumSubscribers() > 0;
   ir_subscribers_ = this->count_subscribers("ir/image") > 0 ||
                     this->count_subscribers("ir/camera_info") > 0;
 
